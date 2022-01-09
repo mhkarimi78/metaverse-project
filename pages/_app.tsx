4de60a28e60b43eb51b411d8e4cd4d05e2fcdf22
mahsa <mahsa_karim16@yahoo.com>
2022-01-09 19:52:21 +0330

    login flow with meralos

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { MoralisProvider } from "react-moralis";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <MoralisProvider
+      children={undefined}
+      appId={process.env.NEXT_PUBLIC_APP_ID}
+      serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}
+    >
+      <Component {...pageProps} />
+    </MoralisProvider>
+  );
+}
+
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import Login from "../components/Login";
+import { useMoralis } from "react-moralis";
+
+export default function Home() {
+  const { isAuthenticated, logout } = useMoralis();
+  if (!isAuthenticated) return <Login />;
+
+  return (
+    <div className="h-screen">
+      <Head>
+        <title>Metaverse challenge</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <button onClick={logout}>Logout</button>
+    </div>
+  );
+}
