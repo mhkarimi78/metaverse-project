327f853e243b48fd7870019c200f87fbe57cd44e
mahsa <mahsa_karim16@yahoo.com>
2022-01-15 16:37:17 +0330

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,7 @@
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
 import { MoralisProvider } from "react-moralis";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }) {
   return (
     <MoralisProvider
       children={undefined}
